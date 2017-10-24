aabfbaa41f24c7a155465ba3418e4c501757dd3e
liupeng <524570375@qq.com>
2017-10-24 05:10:58 +0000

    [FIX] fix pip8

--- flask_wtf_storage/__init__.py
+++ flask_wtf_storage/__init__.py
@@ -6,4 +6,7 @@ from .utils import upload_file
 __version__ = '0.0.1'
 __author__ = 'liupeng'
 
-__all__ = ['FileField', 'MultipleFileField', 'StorageForm', 'upload_file']
+__all__ = [
+    'FileField', 'MultipleFileField', 'FileDisplayField',
+    'StorageForm', 'upload_file'
+]
